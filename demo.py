043b55aeac1db6eeb9f4eed210eca4db9b807ef5
Sumanta Das <sumantad827@gmail.com>
2022-11-21 20:14:24 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+# python program input two number and swap them
+
+userinput = int(input("enter 1st number : "))
+userinput1 = int(input("enter 2nd number : "))
+
+print('you enter : ', userinput, ",", userinput1)
+
+userinput, userinput1 = userinput1, userinput
+
+print('after swapping  : ', userinput, ',', userinput1)
